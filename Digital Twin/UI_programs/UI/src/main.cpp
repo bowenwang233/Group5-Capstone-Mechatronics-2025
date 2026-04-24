654965af9bba07269dbfb89bf4a8e4298ad52391
suitore <suitore@mcmaster.ca>
2026-04-24 15:20:24 -0400

    Delete Digital Twin/UI_programs/UI directory

--- Digital Twin/UI_programs/UI/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <QApplication>
-#include "dashboard_window.h"
-
-int main(int argc, char *argv[]) {
-    QApplication a(argc, argv);
-    DashboardWindow w;
-    w.show();
-    return a.exec();
-}
